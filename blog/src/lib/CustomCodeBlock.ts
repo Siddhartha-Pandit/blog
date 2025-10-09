b0667bb0d172978d9b0f79f6215762f2fced9b17
Siddhartha <siddharthapandit66@gmail.com>
2025-10-09 14:56:54 +0545

    start the new version of blog

--- blog/src/lib/CustomCodeBlock.ts
+++
@@ -1,9 +0,0 @@
-// CustomCodeBlock.ts
-import { CodeBlockLowlight } from '@tiptap/extension-code-block-lowlight';
-import { ReactNodeViewRenderer } from '@tiptap/react';
-import CustomCodeBlockComponent from '@/components/CustomCodeBlockComponent';
-export const CustomCodeBlock = CodeBlockLowlight.extend({
-  addNodeView() {
-    return ReactNodeViewRenderer(CustomCodeBlockComponent);
-  },
-});
--- blog/src/types/index.ts
+++
@@ -1,8 +0,0 @@
-// types/index.ts
-export type ContentNode = {
-    type: string;
-    attrs?: Record<string, any>;
-    content?: ContentNode[];
-    marks?: { type: string }[];
-    text?: string;
-  };
